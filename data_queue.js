78b8abd26dee56840f4b2fcdff68c22a91a2cdd2
Charlie <charjhall777@gmail.com>
2022-01-28 16:32:01 -0500

    create_db and initial endpoints for server

--- data_queue.js
+++ data_queue.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { uuid } = require('uuidv4');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+	host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.WIKI_USER,
+    password: process.env.PASSWORD,
+    insecureAuth: false
+});
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/send_data", (req, res) => {
+app.post("/signup_user", (req, res) => {
+	// takes in req.body:
+	// age, gender, race, education_level
+	let user_data = req.body;
+
+	// create a unique_id for the user:
+	// this assumes the user will save this uuid on their chrome browser
+	let user_unique_id = uuid();
+
+	// insert as new user
+	connection.query("INSERT INTO user (unique_id, age, gender, race, education_level) VALUES (?, ?, ?, ?, ?)",
+		[user_unique_id, user_data.age, user_data.gender, user_data.race, user_data.education_level], (err) => {
+			if (err)
+				console.error(err);
+
+			res.end(user_unique_id);
+		});
+});
+
+app.get("/pull_view_data", (res, res) => {
+	// expects user_unique_id in req.body
+	let user_unique_id = req.body.user_unique_id;
+	if (!user_unique_id)
+		return res.end();
+
+	connection.query("SELECT viewed_page, voted_page FROM user WHERE unique_id=?;", user_unique_id, (err, result) => {
+		if (err)
+			console.error(err);
+
+		res.json(result);
+	});
+});
+
+app.post("/send_data", (req, res) => {
+	// receive data about a specific page from the user
+
 	console.log(req.body);
 });
 
---
+++ sql_query/CREATE_DB.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS wiki_data;
+
+CREATE DATABASE wiki_data;
+USE wiki_data;
+
+/*
+	MAKE SURE YOU CREATE A USER:
+
+	CREATE USER "name_of_user"@"localhost" IDENTIFIED BY "password";
+
+	GRANT SELECT, UPDATE, INSERT ON wiki_data.* TO "name_of_user"@"localhost";
+	FLUSH PRIVILEGES;
+*/
+
+CREATE TABLE user (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	unique_id VARCHAR(32) NOT NULL,
+	age INT NOT NULL,
+	gender VARCHAR(32),
+	race VARCHAR(64),
+	education_level INT,
+
+	viewed_page INT DEFAULT 0,
+	voted_page INT DEFAULT 0
+);
+
+CREATE TABLE page (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	unique_id VARCHAR(20) NOT NULL,
+	wiki_page TEXT
+);
+
+CREATE TABLE view_vote (
+	user_id INT NOT NULL,
+	page_id INT NOT NULL,
+
+	vote INT,
+
+	page_open DATETIME,
+	page_vote DATETIME,
+	focus_time BIGINT
+);
